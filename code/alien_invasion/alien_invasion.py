83ff694130b0c8699eb6ceb073c0a96fe417dc54
CristianoTom <warr999@163.com>
2026-01-08 13:54:54 +0800

    add code

---
+++ code/alien_invasion/alien_invasion.py
@@ -0,0 +1,20 @@
+import sys 
+import pygame 
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game(): 
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+                                       ai_settings.screen_height)) 
+    pygame.display.set_caption("Alien Invasion") 
+    ship = Ship(ai_settings, screen)      # 创建一艘飞船实例
+    # 开始游戏的主循环
+    while True:                  
+        gf.check_events(ship)                    # 监视键盘和鼠标事件
+        ship.update()                            # 更新飞船的位置
+        gf.update_screen(ai_settings, screen, ship)   # 更新屏幕上的图像  
+run_game()
