0ec8e42eaf14badfdf5ee4551505995e1109576f
CristianoTom <warr999@163.com>
2026-03-20 14:55:33 +0800

    update

--- code/cfd/Burger Equation.py
+++ code/cfd/Burger Equation.py
@@ -10,11 +10,12 @@ phiprime = phi.diff(x)
 
 from sympy.utilities.lambdify import lambdify
 u = -2 * nu * (phiprime / phi) + 4
+ufunc = lambdify((t, x, nu), u)
 
 ###variable declarations
 nx = 101
 nt = 100
-dx = 2 * numpy.pi / (nx - 1)
+dx = 2 * np.pi / (nx - 1)
 nu = .07
 dt = dx * nu
 
@@ -22,7 +23,23 @@ x = np.linspace(0, 2 * np.pi, nx)
 un = np.empty(nx)
 t = 0
 
+
 u = np.asarray([ufunc(t, x0, nu) for x0 in x])
-u
+
+plt.figure(figsize=(11, 7), dpi=100)
+plt.plot(x, u, marker='o', lw=2)
+plt.xlim([0, 2 * np.pi])
+plt.ylim([0, 10]);
+
+for n in range(nt):
+    un = u.copy()
+    for i in range(1, nx-1):
+        u[i] = un[i] - un[i] * dt / dx *(un[i] - un[i-1]) + nu * dt / dx**2 *\
+                (un[i+1] - 2 * un[i] + un[i-1])
+    u[0] = un[0] - un[0] * dt / dx * (un[0] - un[-2]) + nu * dt / dx**2 *\
+                (un[1] - 2 * un[0] + un[-2])
+    u[-1] = u[0]
+        
+u_analytical = np.asarray([ufunc(nt * dt, xi, nu) for xi in x])
 
 
