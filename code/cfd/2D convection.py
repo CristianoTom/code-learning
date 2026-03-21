4eaa12204f1d8794dc9123f3f1508e69561c1a4c
CristianoTom <warr999@163.com>
2026-03-21 10:04:18 +0800

    update

--- code/cfd/2D convection.py
+++ code/cfd/2D convection.py
@@ -3,44 +3,69 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # set up initial conditions
-nx = 81
-ny = 81
-nt = 100
+nx = 101
+ny = 101
+nt = 80
 c = 1
 dx = 2 / (nx - 1)
 dy = 2 / (ny - 1)
-sigma = 0.2
+sigma = .2
 dt = sigma * dx
 
 x = np.linspace(0, 2, nx)
 y = np.linspace(0, 2, ny)
 
-u = np.ones((ny, nx))
-# un = np.ones((ny, nx))
+u = np.ones((ny, nx)) ##create a 1xn vector of 1's
+v = np.ones((ny, nx))
+un = np.ones((ny, nx))
+vn = np.ones((ny, nx))
 
-## Assign initial conditions
-u[int(.5 / dy):int(1 / dy + 1),int(.5 / dx):int(1 / dx + 1)] = 2 
+###Assign initial conditions
+##set hat function I.C. : u(.5<=x<=1 && .5<=y<=1 ) is 2
+u[int(.5 / dy):int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
+##set hat function I.C. : v(.5<=x<=1 && .5<=y<=1 ) is 2
+v[int(.5 / dy):int(1 / dy + 1), int(.5 / dx):int(1 / dx + 1)] = 2
 
-###Plot Initial Condition
+
 fig = plt.figure(figsize=(11, 7), dpi=100)
-ax = fig.add_subplot(111, projection='3d')  # create a 3D plot
-X, Y = np.meshgrid(x, y)            
-surf = ax.plot_surface(X, Y, u[:], cmap=plt.cm.viridis)
+ax = fig.add_subplot(111, projection = "3d")
+X, Y = np.meshgrid(x, y)
+
+ax.plot_surface(X, Y, u, cmap=plt.cm.viridis, rstride=2, cstride=2)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
 plt.show()
 
 
-# calculate 
-for n in range(nt + 1): 
+for n in range(nt + 1): ##loop across number of time steps
     un = u.copy()
-    u[1:, 1:] = (un[1:, 1:] - (c*dt/dx*(un[1:, 1:] - un[1:, :-1])) -
-                              (c*dt/dy*(un[1:, 1:] - un[:-1, 1:])))
+    vn = v.copy()
+    u[1:, 1:] = (un[1:, 1:] - 
+                 (un[1:, 1:] * c * dt / dx * (un[1:, 1:] - un[1:, :-1])) -
+                  vn[1:, 1:] * c * dt / dy * (un[1:, 1:] - un[:-1, 1:]))
+    v[1:, 1:] = (vn[1:, 1:] -
+                 (un[1:, 1:] * c * dt / dx * (vn[1:, 1:] - vn[1:, :-1])) -
+                 vn[1:, 1:] * c * dt / dy * (vn[1:, 1:] - vn[:-1, 1:]))
+    
     u[0, :] = 1
     u[-1, :] = 1
     u[:, 0] = 1
     u[:, -1] = 1
+    
+    v[0, :] = 1
+    v[-1, :] = 1
+    v[:, 0] = 1
+    v[:, -1] = 1
+
 
 fig = plt.figure(figsize=(11, 7), dpi=100)
-ax = plt.subplot(111, projection='3d')  # create a 3D plot
-surf1 = ax.plot_surface(X, Y, u[:], cmap=plt.cm.viridis)
-
+ax = fig.add_subplot(111, projection = "3d")
+X, Y = np.meshgrid(x, y)
+ax.plot_surface(X, Y, v, cmap=plt.cm.viridis, rstride=2, cstride=2)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
 plt.show()
+
+
+
+
