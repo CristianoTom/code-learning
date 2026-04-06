66a197784db02b0dd1ffefc302d2d62412f988a0
CristianoTom <warr999@163.com>
2026-04-06 20:28:03 +0800

    update

--- fengzhuang/function.py
+++ fengzhuang/function.py
@@ -16,7 +16,6 @@ class LinearNet(nn.Module):
     def __init__(self, n_input, in_output):
         super(LinearNet, self).__init__()
         self.linear = nn.Linear(n_input, in_output)
-
     def forward(self, x):
         y = self.linear(x)
         return y
@@ -33,3 +32,20 @@ def load_array(data_arrays, batch_size, is_train=True):
     """构造一个PyTorch数据迭代器"""
     dataset = data.TensorDataset(*data_arrays)
     return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+def train(net, features, labels, batch_size, num_epochs, lr):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net.to(device)
+    loss = nn.MSELoss()
+    trainer = torch.optim.SGD(net.parameters(), lr=lr)
+    data_iter = load_array((features, labels), batch_size)
+    for epoch in range(num_epochs):
+        net.train()
+        for X, y in data_iter:
+            l = loss(net(X) ,y)
+            trainer.zero_grad()
+            l.backward()
+            trainer.step()
+        l = loss(net(features), labels)
+        print(f'epoch {epoch + 1}, loss {l:f}')
--- fengzhuang/main.py
+++ fengzhuang/main.py
@@ -3,4 +3,5 @@ import function
 
 net_structure = [10, 5, 1]
 net = function.get_net(net_structure)
-print(net)
+
+
